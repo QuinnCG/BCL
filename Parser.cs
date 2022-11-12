aae07acd105e81959cf50093084e0dd6030eca7f
Quinton <86534924+QuinnCG@users.noreply.github.com>
2022-11-11 20:35:32 -0500

    Add project files.

---
+++ Parser.cs
@@ -0,0 +1,32 @@
+﻿namespace BCL;
+
+// This class breaks a bunch of source text down into "Expression" objects.
+internal class Parser
+{
+	// A list of all the expressions the parser obtains.
+	// An expression here means a line of code.
+	public List<Expression> Expressions { get; set; } = new();
+
+	public Parser(string source)
+	{
+		// Seperate the source code by the newline character.
+		string[] lines = source.Split(Environment.NewLine);
+
+		foreach (var line in lines)
+		{
+			// Seperate the line by its spaces.
+			string[] parts = line.Split(' ');
+
+			// The first part of the line is the command name.
+			string cmd = parts[0];
+			// Every part after the name is the command arguments.
+			string[] args = parts[1..];
+
+			// Add expressions who's command name is "var", "call", or "label".
+			if (cmd is "var" or "call" or "label")
+			{
+				Expressions.Add(new Expression(cmd, args));
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// BCL stands for "Basic Command Language".
+// Its similar in design to writing console commands in Command Prompt.
+// Each line is a command. Parts of a command are seperated by spaces. The first part is the command's name and the rest are it's arguments.
+// BCL compiles to CIL; Microsoft's "Common Intermediate Language".
+// CIL is a stack-based, object-oriented, assembly language that languages such as C# compile into to.
+
+using System.Diagnostics;
+
+namespace BCL;
+
+internal static class Program
+{
+	static void Main()
+	{
+		string filename;
+		while(true)
+		{
+			Console.WriteLine("Enter file to compile.");
+			var input = Console.ReadLine();
+
+			if (File.Exists(input))
+			{
+				// Make sure the file extension is ".bcl".
+				if (input.Split('.')[1] is "bcl")
+				{
+					filename = input;
+					break;
+				}
+			}
+
+			Console.Clear();
+		}
+
+		string source = File.ReadAllText(filename);
+		Parser parser;
+		Generator generator;
+
+		try
+		{
+			parser = new Parser(source);
+			generator = new Generator(filename.Split('.')[0], parser.Expressions);
+		}
+		catch (Exception e)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("[Compiler Error]: ");
+			Console.WriteLine(e.Message);
+			Console.WriteLine("Press any key to close...");
+			Console.ReadKey(true);
+			return;
+		}
+
+		string filepath = filename.Split('.')[0] + ".il";
+		using var file = File.CreateText(filepath);
+		file.Write(generator.Builder.ToString());
+		file.Close();
+
+		// Run "ilasm.exe" (the CIL compiler) on the generated ".il" file.
+		using var ilProcess = Process.Start("ilasm.exe", filepath);
+		ilProcess.WaitForExit();
+
+		Console.WriteLine("Press any key to close...");
+		Console.ReadKey(true);
+	}
+}
